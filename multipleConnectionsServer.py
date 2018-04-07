e06ff2db0fad8f0e1f65d9906721d53adf317204
Madhav Datt <madhav.datt@hotmail.com>
2018-04-07 13:38:29 +0530

    multiple connect

--- multipleConnectionsServer.py
+++ multipleConnectionsServer.py
@@ -20,15 +20,15 @@ inputs = [server]
 
 # Sockets to which we expect to write
 outputs = []
-
-# Outgoing message queues (socket:Queue)
-message_queues = {}
+#
+# # Outgoing message queues (socket:Queue)
+# message_queues = {}
 
 while inputs:
 
     # Wait for at least one of the sockets to be ready for processing
     print(sys.stderr, '\nwaiting for the next event')
-    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+    readable, _, _ = select.select(inputs, outputs, inputs)
 
     # Handle inputs
     for s in readable:
@@ -41,51 +41,56 @@ while inputs:
             inputs.append(connection)
 
             # Give the connection a queue for data we want to send
-            message_queues[connection] = queue.Queue()
+            # message_queues[connection] = queue.Queue()
         else:
+            # TODO Read the whole message. Covert to Message object from pickle
+            # TODO Call handler according to message type.
+            # TODO Socket option in send_message function.
+            # TODO Add message handlers and types etc. ***
             data = s.recv(1024)
             if data:
-                # A readable client socket has data
-                print(sys.stderr, 'received "%s" from %s' % (
-                    data, s.getpeername()))
-                message_queues[s].put(data)
-                # Add output channel for response
-                if s not in outputs:
-                    outputs.append(s)
-            else:
-                # Interpret empty result as closed connection
-                print(sys.stderr, 'closing', client_address,
-                      'after reading no data')
-                # Stop listening for input on the connection
-                if s in outputs:
-                    outputs.remove(s)
-                inputs.remove(s)
-                s.close()
+                print(data)
+                # # A readable client socket has data
+                # print(sys.stderr, 'received "%s" from %s' % (
+                #     data, s.getpeername()))
+                # message_queues[s].put(data)
+                # # Add output channel for response
+                # if s not in outputs:
+                #     outputs.append(s)
+            # else:
+            #     # Interpret empty result as closed connection
+            #     print(sys.stderr, 'closing', client_address,
+            #           'after reading no data')
+            #     # Stop listening for input on the connection
+            #     if s in outputs:
+            #         outputs.remove(s)
+            #     inputs.remove(s)
+            #     s.close()
+            #
+            #     # Remove message queue
+            #     del message_queues[s]
 
-                # Remove message queue
-                del message_queues[s]
-
-    # Handle outputs
-    for s in writable:
-        try:
-            next_msg = message_queues[s].get_nowait()
-        except queue.Empty:
-            # No messages waiting so stop checking for writability.
-            print(sys.stderr, 'output queue for', s.getpeername(), 'is empty')
-            outputs.remove(s)
-        else:
-            print(
-                sys.stderr, 'sending "%s" to %s' % (next_msg, s.getpeername()))
-            s.send(next_msg)
-
-    # Handle "exceptional conditions"
-    for s in exceptional:
-        print(sys.stderr, 'handling exceptional condition for', s.getpeername())
-        # Stop listening for input on the connection
-        inputs.remove(s)
-        if s in outputs:
-            outputs.remove(s)
-        s.close()
-
-        # Remove message queue
-        del message_queues[s]
+    # # Handle outputs
+    # for s in writable:
+    #     try:
+    #         next_msg = message_queues[s].get_nowait()
+    #     except queue.Empty:
+    #         # No messages waiting so stop checking for writability.
+    #         print(sys.stderr, 'output queue for', s.getpeername(), 'is empty')
+    #         outputs.remove(s)
+    #     else:
+    #         print(
+    #             sys.stderr, 'sending "%s" to %s' % (next_msg, s.getpeername()))
+    #         s.send(next_msg)
+    #
+    # # Handle "exceptional conditions"
+    # for s in exceptional:
+    #     print(sys.stderr, 'handling exceptional condition for', s.getpeername())
+    #     # Stop listening for input on the connection
+    #     inputs.remove(s)
+    #     if s in outputs:
+    #         outputs.remove(s)
+    #     s.close()
+    #
+    #     # Remove message queue
+    #     del message_queues[s]
