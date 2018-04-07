6b5b9140eab733741071cc9e1df85ca262604956
Anurag <chatrasen@gmail.com>
2018-04-07 12:41:03 +0530

    Updated server.py to make it able to accept connections from an outside client

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket                       # Import socket module
 s = socket.socket()                 # Create a socket object
 host = socket.gethostname()         # Get local machine name
 port = 12345                        # Reserve a port for your service.
-s.bind((host, port))                # Bind to the port
+s.bind(('', port))                # Bind to the port
 f = open('recv.png','wb')
 s.listen(5)                         # Now wait for client connection.
 while True:
