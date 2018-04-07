bd8c5a2bbe048a592ffa27fb89c2bda5efc96dea
Anurag <chatrasen@gmail.com>
2018-04-07 13:27:27 +0530

    made client python3

--- multipleClientTest.py
+++ multipleClientTest.py
@@ -13,7 +13,7 @@ socks = [ socket.socket(socket.AF_INET, socket.SOCK_STREAM),
           ]
 
 # Connect the socket to the port where the server is listening
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+print(sys.stderr, 'connecting to %s port %s' % server_address)
 for s in socks:
     s.connect(server_address)
 
@@ -21,13 +21,13 @@ for message in messages:
 
     # Send messages on both sockets
     for s in socks:
-        print >>sys.stderr, '%s: sending "%s"' % (s.getsockname(), message)
-        s.send(message)
+        print(sys.stderr, '%s: sending "%s"' % (s.getsockname(), message))
+        s.send(bytes(message,'UTF-8'))
 
     # Read responses on both sockets
     for s in socks:
         data = s.recv(1024)
-        print >>sys.stderr, '%s: received "%s"' % (s.getsockname(), data)
+        print(sys.stderr, '%s: received "%s"' % (s.getsockname(), data))
         if not data:
-            print >>sys.stderr, 'closing socket', s.getsockname()
+            print(sys.stderr, 'closing socket', s.getsockname())
             s.close()
