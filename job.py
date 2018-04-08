06ee71831af7cc37ad81f27b1dadc3a18a41d675
Avikalp7 <avikalp22@gmail.com>
2018-04-08 15:33:30 +0530

    edit job and jobparser

--- job.py
+++ job.py
@@ -1,5 +1,5 @@
 class Job:
-	def __init__(self, name, executable, priority, time_required, min_memory, min_cores, preference):
+	def __init__(self, name, executable, priority, time_required, min_memory, min_cores, max_memory):
 		"""
 		:param name: str, name of the job
 		:param executable: str, address of the executable file
@@ -7,7 +7,7 @@ class Job:
 		:param time_required: int, seconds of time required
 		:param min_memory: int, min amount of memory (in MB) required for execution
 		:param min_cores: int, min no. of cores required for execution
-		:param preference: str, preference formula calculation as combination of memory & core.
+		:param preference: int, max amount of memory (in MB) preferred for execution
 		"""
 		self.name = name
 		self.username = os.uname()[1]
@@ -18,4 +18,4 @@ class Job:
 		# Requirements
 		self.min_memory = min_memory
 		self.min_cores = min_cores
-		self.preference = preference
+		self.max_memory = p
